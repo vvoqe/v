ec2bd0b726776c3f765ea43aed819ab9005ebd5e
v <arnizard@proton.me>
2025-04-20 17:35:59 +0200

    Update bio.js

--- js/bio.js
+++ js/bio.js
@@ -1,7 +1,7 @@
 function sleep(e) {
     return new Promise((s => setTimeout(s, e)))
 }
-const phrases = ["Spain | 18", "Project Manager", "Animation Comms"]
+const phrases = ["Spain | 18", "Staff | Management"]
   , el = document.getElementById("bio");
 let sleepTime = 150
   , curPhraseIndex = 0;
